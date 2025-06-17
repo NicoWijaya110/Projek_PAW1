fc2d9c85100ad50aac680345acb5063d3e33ed45
Michael Jovanco <michaeljovanco_2327240023@mhs.mdp.ac.id>
2025-06-17 11:59:14 +0700

    perbaiki breeze

--- routes/webbackup.php
+++ routes/webbackup.php
@@ -1,26 +1,24 @@
 <?php
 
 use App\Http\Controllers\AbsenController;
+use App\Http\Controllers\AbsensiController;
 use App\Http\Controllers\JadwalController;
 use App\Http\Controllers\MateriController;
+use App\Models\Jadwal;
+use App\Models\Materi;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\DosenController;
 use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\NilaiController;
-use App\Http\Controllers\ProfileController;
+use App\Models\Absensi;
+use App\Models\Nilai;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 Route::get('/', function () {
     return view('welcome');
-})->name('welcome');
-
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
 Route::resource('materi', MateriController::class);
 Route::resource('jadwal',JadwalController::class);
 Route::resource('dashboard', DashboardController::class);
@@ -28,5 +26,4 @@ Route::resource('dosen',DosenController::class);
 Route::resource('mahasiswa', MahasiswaController::class);
 Route::resource('nilai',NilaiController::class);
 Route::resource('absen', AbsenController::class);
-
-require __DIR__.'/auth.php';
+Route::get('/rekap-absensi', [AbsenController::class, 'rekap'])->name('absen.rekap');
